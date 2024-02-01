79e06b6d5fc36e608463c286a313745e3cdc5a8e
Dan <flyjeray.dev@gmail.com>
2024-02-01 09:48:42 +0700

    [back] delete post when user is deleted

--- backend/models.py
+++ backend/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, ARRAY, DateTime
+from sqlalchemy import Column, Integer, String, ARRAY, DateTime, ForeignKey
 from sqlalchemy.sql import func
 from database import Base
 
@@ -15,5 +15,5 @@ class Post(Base):
     id = Column(Integer, primary_key=True, index=True)
     title = Column(String, index=True)
     text = Column(String, index=True)
-    owner = Column(Integer, index=True)
+    owner = Column(Integer, ForeignKey(User.id, ondelete="CASCADE"), index=True)
     created_at = Column(DateTime(timezone=True), server_default=func.now())

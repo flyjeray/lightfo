3e9d4a069376213ab39bbd364e60871d9d3325d6
Dan <flyjeray.dev@gmail.com>
2024-02-05 14:01:00 +0800

    [back] add /posts/delete

--- backend/routers/posts.py
+++ backend/routers/posts.py
@@ -19,6 +19,9 @@ class CreatePostPayload(BaseModel):
 class CreatePostResponse(BaseModel):
     id: int
 
+class MessageResponse(BaseModel):
+    message: str
+
 @router.post('/create', status_code=201, response_model=CreatePostResponse, summary="Create a new post")
 def create_post(data: CreatePostPayload, db: db_dependency, creds: HTTPAuthorizationCredentials = Depends(token_auth_scheme)):
     token_data = auth_utils.verify_token_access(creds.credentials)
@@ -36,4 +39,20 @@ def create_post(data: CreatePostPayload, db: db_dependency, creds: HTTPAuthoriza
         db.commit()
 
         return { "id": new_post.id }
+    
+@router.delete("/delete", status_code=200, response_model=MessageResponse, summary="Delete an existing post")
+def delete_post(id: int, db: db_dependency, creds: HTTPAuthorizationCredentials = Depends(token_auth_scheme)):
+    token_data = auth_utils.verify_token_access(creds.credentials)
+
+    post = db.query(models.Post).where(models.Post.id == id).first()
+
+    if post is None:
+        raise HTTPException(status_code=404, detail="Post is not found")
+    elif str(post.owner) != token_data.id:
+        raise HTTPException(status_code=401, detail="You have no access to modify this post")
+    else:
+        db.delete(post)
+        db.commit()
+
+    return { "message": "success" }
 
